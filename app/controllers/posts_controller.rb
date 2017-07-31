60968f2ba4a2d31ccb4735b1561e973f680a6496
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-31 09:50:34 -0400

    section 2

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  #index
+  def index
+    @posts = Post.all
+  end
+
+  #new
+  def new
+    @post = Post.new
+  end
+  #create
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
+  end
+
+  #show
+  def show
+    @post = Post.find(params[:id])
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content, :is_published)
+  end
+end
